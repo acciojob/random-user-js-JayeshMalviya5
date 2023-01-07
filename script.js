6c82abfa4f9b568c848f23521a51d3a9ee22cb84
JayeshMalviya5 <115400817+JayeshMalviya5@users.noreply.github.com>
2023-01-07 21:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-//your code here
+
+      const userInfoDiv = document.getElementById("user-info");
+      const additionalInfoDiv = document.getElementById("additional-info");
+      const buttons = document.querySelectorAll("button");
+      let currentUser;
+
+      // fetch a random user when the page loads
+      fetch("https://randomuser.me/api/")
+        .then(response => response.json())
+        .then(data => {
+          currentUser = data.results[0];
+          // display the user's name and image
+          userInfoDiv.innerHTML = `
+            <h2>${currentUser.name.first} ${currentUser.name.last}</h2>
+            <img src="${currentUser.picture.large}" alt="Profile picture">
+          `;
+        });
+
+      // handle clicks on the buttons
+      buttons.forEach(button => {
+        button.addEventListener("click", event => {
+          const attr = event.target.getAttribute("data-attr");
+          let info;
+          if (attr === "age") {
+            info = currentUser.dob.age;
+          } else if (attr === "email") {
+            info = currentUser.email;
+          } else if (attr === "phone") {
+            info = currentUser.phone;
+          }
+          additionalInfoDiv.innerHTML = `<p>${attr}: ${info}</p>`;
+        });
+      });
+
+      // handle clicks on the "Get New User" button
+      const getUserButton = document.getElementById("getUser");
+      getUserButton.addEventListener("click", () => {
+        fetch("https://randomuser.me/api/")
+          .then(response => response.json())
+          .then(data => {
+            currentUser = data.results[0];
+            // display the user's name and image
+            userInfoDiv.innerHTML = `
+              <h2>${currentUser.name.first} ${currentUser.name.last}</h2>
+              <img src="${currentUser.picture.large}" alt="Profile picture">
+            `;
+            // clear the additional info
+            additionalInfoDiv.innerHTML = "";
+          });
+      });
+    
